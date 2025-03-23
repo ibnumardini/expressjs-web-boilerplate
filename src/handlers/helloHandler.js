c4c23f105be34d617e7ad2dab7b7e15096dbdfd3
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2025-03-23 09:49:27 +0700

    refactor: rename coupon service import to helloService for clarity

--- src/handlers/helloHandler.js
+++ src/handlers/helloHandler.js
@@ -1,9 +1,9 @@
-import * as coupon from '../services/helloService.js';
+import * as helloService from '../services/helloService.js';
 
 export const index = async (req, res) => {
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
-  const { hello } = await coupon.sayHello(ip);
+  const { hello } = await helloService.sayHello(ip);
 
   res.render('hello/index', { title: 'Hello', hello: `${hello} ✋🏻` });
 };
